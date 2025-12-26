b91a3470ec4a88b331f7d4e0965b3c4a570d186b
Ocean Thunder <mrveganwatermelon@proton.me>
2025-12-26 09:09:41 +0000

    updated LICENSE

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="textmonger",
-    version="1.0.3",
+    version="1.0.4",
     project_urls={
         "Source": "https://github.com/oceanthunder/textmonger",
     },
@@ -19,7 +19,7 @@ setup(
     ],
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
+        "License :: OSI Approved :: The Unlicense (Unlicense)",
         "Operating System :: OS Independent",
     ],
     python_requires=">=3.6",
